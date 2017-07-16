6919003551c4873753dbce2cc511fac59f267ce2
Adam Rainsby <adamrainsby@live.co.uk>
2017-07-17 00:30:56 +0100

    Add VCR fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,29 @@ VCR.configure do |c|
   c.filter_sensitive_data('<authorization code>') do
     Base64.strict_encode64("#{ENV['TEST_BRIVO_CLIENT_ID']}:#{ENV['TEST_BRIVO_SECRET']}")
   end
+
+  c.filter_sensitive_data('<access token>') do |interaction|
+    begin
+      body = JSON.parse(interaction.response.body)
+      body.dig('access_token')
+    rescue JSON::ParserError
+    end
+  end
+
+  c.filter_sensitive_data('<refresh token>') do |interaction|
+    begin
+      body = JSON.parse(interaction.response.body)
+      body.dig('refresh_token')
+    rescue JSON::ParserError
+    end
+  end
+
+  c.filter_sensitive_data('<access token>') do |interaction|
+    authorization_header = interaction.request.headers['Authorization'].first
+    if authorization_header.include? 'bearer'
+      authorization_header.gsub('bearer ', '')
+    end
+  end
 end
 
 module Brivo::TestClient
